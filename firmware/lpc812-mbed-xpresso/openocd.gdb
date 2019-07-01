aff64958ea5a64be826e3da151c219d41bc9064d
Martin Atkins <mart@degeneration.co.uk>
2019-06-30 19:36:31 -0700

    lpc812-mbed-xpresso: blink green LED
    This is just to see that the peripheral-access crate is functioning. There
    doesn't seem to be a HAL crate for this platform yet, so this is just
    doing unsafe writes directly to the mapped I/O ports.

--- firmware/lpc812-mbed-xpresso/openocd.gdb
+++ firmware/lpc812-mbed-xpresso/openocd.gdb
@@ -9,9 +9,7 @@ set print asm-demangle on
 # set backtrace limit to not have infinite backtrace loops
 set backtrace limit 32
 
-# detect unhandled exceptions, hard faults and panics
-break DefaultHandler
-break HardFault
+# break if we hit a panic
 break rust_begin_unwind
 
 # try to stop at the user entry point
--- firmware/lpc812-mbed-xpresso/src/main.rs
+++ firmware/lpc812-mbed-xpresso/src/main.rs
@@ -7,12 +7,24 @@ extern crate panic_halt;
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use lpc81x_pac;
 
 #[entry]
 fn main() -> ! {
-    asm::nop();
+    let p = lpc81x_pac::Peripherals::take().unwrap();
+
+    p.GPIO_PORT
+        .dir0
+        .modify(|r, w| unsafe { w.bits(r.bits() | 1 << 17) });
+    p.GPIO_PORT
+        .dir0
+        .modify(|r, w| unsafe { w.bits(r.bits() | 1 << 17) });
 
     loop {
-        asm::wfi();
+        p.GPIO_PORT.not0.write(|w| unsafe { w.bits(1 << 17) });
+        for _ in 0..1000 {
+            asm::nop();
+        }
+        //asm::wfi();
     }
 }
